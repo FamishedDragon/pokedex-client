d53f41c1f5ea641763fcfad29058ae263e889410
Robert <crossbreed@outlook.com>
2022-01-17 19:56:02 -0800

    Fixing poke-card style and colors
    Updates to the poke card aesthetic

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 const pokeContainer = document.getElementById("poke-container");
-const pokemonNum = 25;
+const pokemonNum = 150;
 
 const typeColors = {
     fire: '#e9a353',
     grass: '#65d16c',
-    electric: '#faea5c',
+    electric: '#fffc42',
     water: '#509ec2',
-    fairy: '#d46a96',
+    fairy: '#ffb9d6',
     ghost: '#da8ff1',
     dark: '#747474',
     steel: '#8d8b9e',
     ice: '#7c99ce',
-    ground: '#b96816',
-    rock: '#646464',
     poison: '#9563c5',
+    rock: '#999999',
+    ground: '#bba374',
     bug: '#9bc07c',
-    dragon: '#e9c915',
-    psychic: '#c91962',
+    dragon: '#ffdc6a',
+    psychic: '#ff7195',
     flying: '#b8e5fa',
-    fighting: '#685735',
+    fighting: '#ff5e52',
     normal: '#F5F5F5',
 };
 
 const typeColorsBold = {
     fire: '#d37b16',
     grass: '#197a20',
-    electric: '#e6c005',
+    electric: '#e2d409',
     water: '#0a5a80',
     fairy: '#d46a96',
     ghost: '#9e46b9',
     dark: '#32313a',
     steel: '#434157',
     ice: '#3f68b6',
-    ground: '#685735',
-    rock: '#646464',
     poison: '#8034c7',
+    rock: '#646464',
+    ground: '#685735',
     bug: '#4b6d2e',
-    dragon: '#e9c915',
+    dragon: '#ffc401',
     psychic: '#c91962',
-    flying: '#a0d9f3',
-    fighting: '#685735',
+    flying: '#73bfe2',
+    fighting: '#b92e16',
     normal: '#dddddd',
 };
 
@@ -75,7 +75,8 @@ function createPokemonCard(pokemon) {
     const img = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
 
     // Type
-    let types = pokemon.types[0].type.name;
+    let typeMain = pokemon.types[0].type.name;
+    let typeSecondary = "";
     // We want the BG color to be the prominent type of the mon, not necisarily the first type
     // So we will welect the type that appears first on the list of colors above
     const pokeTypes = pokemon.types.map(element => element.type.name);
@@ -83,13 +84,19 @@ function createPokemonCard(pokemon) {
     const colorBG = typeColors[mainType];
 
     // Color for name plate
-    let colorNameplate = typeColorsBold[types];
+    let colorNameplate = typeColorsBold[mainType];
+    let type1Color = typeColorsBold[typeMain];
+    let type2Color = "";
+    let doubleType = false;
     if (pokemon.types.length > 1) {
         // If pokemon has more than one type, we will alter the name plate and type text to show both
-        types = `${types}/${pokemon.types[1].type.name}`;
-        colorNameplate = `linear-gradient(125deg, ${colorNameplate} 35%, ${typeColorsBold[pokemon.types[1].type.name]} 65%)`;
+        typeSecondary = `${pokemon.types[1].type.name}`;
+        type2Color = typeColorsBold[pokemon.types[1].type.name];
+        //colorNameplate = `linear-gradient(125deg, ${colorNameplate} 35%, ${typeColorsBold[pokemon.types[1].type.name]} 65%)`;
+        doubleType = true;
     }
 
+    // Put everything into HTML
     pokeEl.style.background = colorBG;
     const pokeInnerHTML = `
         <div class="img-container">
@@ -98,10 +105,16 @@ function createPokemonCard(pokemon) {
         <div class="info">
             <span class="number">#${id}</span>
             <h3 class="name" style="background:${colorNameplate}">${name}</h3>
-            <small class="type">Type: <span>${types}</spam><small>
+            <h5 class="type">
+                <span class="type-element" style="background:${type1Color}">${typeMain}</span>
+                ${doubleType 
+                    ? `<span class="type-element" style="background:${type2Color}">  ${typeSecondary}  </span>` 
+                    : ""}
+            <h5>
         </div>
     `;
-
+    
+    // Add HTML into our index.html file
     pokeEl.innerHTML = pokeInnerHTML;
     pokeContainer.appendChild(pokeEl);
 }
